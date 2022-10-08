87198d323dfa18b8c0debc1c9e69dae6810406dd
Benjamin Guichard <benjamin.guichard@epita.fr>
2022-10-08 16:19:16 +0200

    modified iterations through matrices and naming

--- src/utils/matrix.h
+++ src/utils/matrix.h
@@ -7,18 +7,19 @@ struct Matrix
 {
     size_t cols;
     size_t lines;
-    int *matrix;
+    int **matrix;
 };
 
 struct Matrix *init_matrix(size_t cols, size_t lines);
 void print_matrix(struct Matrix* m);
 void fill_matrix(struct Matrix *m, int dbgFlag);
-char *toString(struct Matrix *m);
+int *flatMatrix(struct Matrix *m);
 void clear_matrix(struct Matrix *m);
 void free_matrix(struct Matrix *m);
 int getElement(struct Matrix *m, int i, int j);
 void setElement(struct Matrix *m, int element, int i, int j);
-struct Matrix *copyFromFile(struct Matrix *m, const char *path, int index);
-void copyToFile(struct Matrix *m, const char *path, int index);
+void free_inside_matrix(struct Matrix *m);
+// struct Matrix *copyFromFile(struct Matrix *m, const char *path, int index);
+// void copyToFile(struct Matrix *m, const char *path, int index);
 
 #endif

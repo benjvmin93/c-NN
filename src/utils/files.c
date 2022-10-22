09906130d6656916471cf73fe025a0c2b3461af1
Benjamin Guichard <benjamin.guichard@epita.fr>
2022-10-22 20:08:26 +0200

    get directory names contained inside a directory

--- src/utils/files.c
+++ src/utils/files.c
@@ -33,9 +33,38 @@ char **getFileNamesFromDir(const char *dataPath)
                 err(1, "files.getFleNamesFromDir(): strdup of `%s` failed.", dirent->d_name);
         }
     }
-
+    closedir(dir);
     fileNames = realloc(fileNames, (nb_files + 1) * sizeof(char *));
     fileNames[nb_files] = NULL;
 
     return fileNames;
+}
+
+char **getDirNamesFromDir(const char *dataPath)
+{
+    struct dirent *dirent;
+    DIR *dir = opendir(dataPath);
+
+    if (!dir)
+        err(errno, "Couldn't open directory `%s`.", dataPath);
+    
+    size_t nb_dir = 0;
+    char **dirNames = NULL;
+
+    while ((dirent = readdir(dir)))
+    {
+        if (dirent->d_type == DT_DIR && strcmp(dirent->d_name, ".") && strcmp(dirent->d_name, ".."))
+        {
+            nb_dir++;
+            dirNames = realloc(dirNames, nb_dir * sizeof(char*));
+            dirNames[nb_dir - 1] = strdup(dirent->d_name);
+            if (!dirNames[nb_dir - 1])
+                err(1, "files.getDirNamesFromDir(): strdup of `%s` failed.", dirent->d_name);
+        }
+    }
+    closedir(dir);
+    dirNames = realloc(dirNames, (nb_dir + 1) * sizeof(char *));
+    dirNames[nb_dir] = NULL;
+
+    return dirNames;
 }
--- src/utils/files.h
+++ src/utils/files.h
@@ -2,5 +2,6 @@
 #define FILES_H
 
 char **getFileNamesFromDir(const char *dataPath);
+char **getDirNamesFromDir(const char *dataPath);
 
 #endif

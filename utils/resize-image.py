4c23f0a17315075dfc68702fbc2561d518e5dcb5
Benjamin Guichard <benjamin.guichard@epita.fr>
2022-10-22 02:37:58 +0200

    resize-image script

--- utils/resize-image.py
+++ utils/resize-image.py
@@ -1,6 +1,18 @@
 from PIL import Image
 
+import sys
+
+def usage():
+    print('USAGE: python3 resize-image.py ${IMAGE-TO-RESIZE}')
+    exit(1)
+
 def resize_image(path):
     image = Image.open(path)
     new_image = image.resize((300, 300))
     new_image.save('normalized-images/normalized-' + path)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 1:
+        usage()
+    resize_image(args[1])

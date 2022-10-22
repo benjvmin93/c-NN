f5c665e9f66e661a4383fd5ce86c06ccc74ff61f
Benjamin Guichard <benjamin.guichard@epita.fr>
2022-10-22 02:46:20 +0200

    normalize image script modif

--- utils/resize-image.py
+++ utils/resize-image.py
@@ -1,5 +1,4 @@
 from PIL import Image
-
 import sys
 
 def usage():
@@ -7,9 +6,17 @@ def usage():
     exit(1)
 
 def resize_image(path):
+    split = path.split('/')
+    name = split[len(split) - 1]
+    new_path = 'utils/normalized-images/normalized-' + name
+
     image = Image.open(path)
+    if image.size == (300, 300):
+        image.save(new_path)
+        return 0
     new_image = image.resize((300, 300))
-    new_image.save('normalized-images/normalized-' + path)
+    new_image.save(new_path)
+    return 0
 
 if __name__ == '__main__':
     args = sys.argv
